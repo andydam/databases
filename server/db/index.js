a3e90043b0eff1a2895c62ac5453c47c083db9ba
Andy Dam <andy@andydam.com>
2017-12-24 00:13:21 -0800

    completed mysql db helper functions

--- server/db/index.js
+++ server/db/index.js
@@ -24,108 +24,90 @@ chatDB.queryAsync = function (...args) {
 };
 
 module.exports = {
-  getID(value, table) {
-    //returns id of value if in table, returns false if not in table
+  selectVal(desired, from, where, filter) {
+    //runs a select query on database using passed in arguments
     return new Promise((resolve, reject) => {
-      chatDB.queryAsync('SELECT * FROM ?? WHERE name = ?', [table, value])
+      //run a 'select from where' mysql async query
+      chatDB.queryAsync('SELECT ?? FROM ?? WHERE ?? = ?', [desired, from, where, filter])
         .then(data => {
-          data.length > 0 ? resolve(data[0].id) : resolve(false);
+          //if data is returned, return the data, else return false
+          data.length ? resolve(data[0][desired]) : resolve(false);
         }, err => reject(err));
     });
   },
-  getName(value, table) {
-    //returns name of id if in table, returns false if not in table
+  insertTo(table, key, value) {
+    //inserts data to the database using passed in arguements, returns id inserted at if insert successful 
     return new Promise((resolve, reject) => {
-      chatDB.queryAsync('SELECT * FROM ?? WHERE id = ?', [table, value])
-        .then(data => {
-          data.length > 0 ? resolve(data[0].name) : resolve(false);
-        }, err => reject(err));
-    });
-  },
-  insertTo(value, field, table) {
-    //inserts value to table, returns id if inserted successfully or if already inserted
-    return new Promise((resolve, reject) => {
-      this.getID(value, field, table)
+      //check if data is already in table by attempting to get the id
+      this.selectVal('id', table, key, value)
         .then(status => {
-          return status !== false ? {affectedRows: [status], insertId: status} : chatDB.queryAsync('INSERT INTO ?? (??) VALUES (?)', [table, field, value]);
+          //if able to get id of data, data already exists in database, return id of data
+          //if not able to, run a 'insert into values' mysql async query
+          return status !== false ? {affectedRows: status, insertId: status} : chatDB.queryAsync('INSERT INTO ?? (??) VALUES (?)', [table, key, value]);
         }, err => reject(err))
         .then(data => {
-          return data.affectedRows.length > 0 ? resolve(data.insertId) : reject(data.serverStatus);
+          //if data was successfully added, return the id of data, else return server response
+          data.affectedRows ? resolve(data.insertId) : reject(data.serverStatus);
         }, err => reject(err));
     });
   },
   getUserID(username) {
-    //checks to see if a user exists in the database
-    //passes user id if in database, false if not
-    return this.getID(username, 'users');
-  },
-  addUser(username) {
-    //adds a user to the database
-    //returns user id of new user or user id if user already exists
-    return this.insertTo(username, 'users');
+    //gets user id for a given username, returns false if user is not in database
+    return this.selectVal('id', 'users', 'name', username);
   },
   getUserName(userId) {
-    //gets username from database using user id
-    return this.getName(userId, 'users');
+    //gets username for a given user id, returns false if user is not in database
+    return this.selectVal('name', 'users', 'id',userId);
   },
-  getRoomID(room) {
-    //checks to see if room exists in the database
-    //passes room id if in database, false if not
-    return this.getID(room, 'rooms');
+  addUser(username) {
+    //adds a user to the database, returns id once added
+    return this.insertTo('users', 'name', username);
   },
-  addRoom(room) {
-    //adds a room to the database
-    //returns room id of new room or room id if room already exists
-    return this.insertTo(room, 'rooms');
+  getRoomID(roomname) {
+    //gets room id for a given room name, returns false if room is not in database
+    return this.selectVal('id', 'rooms', 'name', roomname);
   },
   getRoomName(roomId) {
-    //gets roomname from database using room id
-    return this.getName(roomId, 'rooms');
+    //gets room name for a given room id, returns false if room is not in database
+    return this.selectVal('name', 'rooms', 'id', roomId);
+  },
+  addRoom(roomname) {
+    //adds a room to the database, returns id once added
+    return this.insertTo('rooms', 'name', roomname);
   },
   postMessage({text, user, room, time}) {
     //posts a message to the database
     return new Promise((resolve, reject) => {
       this.addUser(user)
         .then(userId => {
+          //check if user is already added to the database by attempting to add it
+          //pull user id
           user = userId;
           return this.addRoom(room);
+          //check if room is already added to the database by attempting to add it
         }, err => reject(err))
         .then(roomId => {
+          //pull room id
           room = roomId;
-          return chatDB.queryAsync('INSERT INTO messages (text, user, room, time) VALUES (?, ?, ?, ?)', [text, user, room, time]);
+          return chatDB.queryAsync('INSERT INTO messages (text, user, room, time) VALUES (?, ?, ?, ?)', [text, Number(user), Number(room), Number(time)]);
         }, err => reject(err))
         .then(data => {
-          data.affectedRows > 0 ? resolve(data.insertId) : reject(data.serverStatus);
+          //add message to database by running a 'insert into values' mysql async query
+          //if message was successfully added, return message id, else return server response
+          data.affectedRows ? resolve(data.insertId) : reject(data.serverStatus);
         }, err => reject(err));
     });
   },
   getMessages() {
-    //get messages from database
-    return new Promise((resolve, reject) => {
-      var users;
-      var rooms;
-      chatDB.queryAsync('select * from users')
-        .then(data => {
-          users = data;
-          return chatDB.queryAsync('select * from rooms');
-        }, err => reject(err))
-        .then(data => {
-          rooms = data;
-          return chatDB.queryAsync('select * from messages');
-        }, err => reject(err))
-        .then(data => {
-          resolve(data.map(message => {
-            message.username = users.filter(user => user.id === message.user)[0].name;
-            message.roomname = rooms.filter(room => room.id === message.room)[0].name;
-            message.id = new Number(message.time);
-            message.createdAt = new Date(message.time).toJSON();
-            message.updatedAt = message.createdAt;
-            delete message.time;
-            delete message.user;
-            delete message.room;
-            return message;
-          }))
-        }, err => reject(err))
-    });
-  }
+    //gets all messages from database by running a 'select from' query on the messages table
+    return chatDB.queryAsync('SELECT * FROM messages');
+  },
+  getUsers() {
+    //gets all users from database by running a 'select from' query on the users table
+    return chatDB.queryAsync('SELECT * from users');
+  },
+  getRooms() {
+    //gets all rooms from database by running a 'select from' query on the rooms table
+    return chatDB.queryAsync('SELECT * from rooms');
+  },
 };
