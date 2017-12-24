d8385c028ee99585f82923e972d28592b9c5b337
Andy Dam <andy@andydam.com>
2017-12-24 00:46:27 -0800

    completed models

--- server/models/index.js
+++ server/models/index.js
@@ -2,18 +2,60 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
-      return db.getMessages();
-    }, // a function which produces all the messages
-    post: function (message) {
-      return db.postMessage(message);
-    } // a function which can be used to insert a message into the database
+    get() {
+      return new Promise((resolve, reject) => {
+        //declare variables to store messages and users
+        let messages = [];
+        let users = [];
+        db.getMessages()
+          .then(data => {
+            //pull messages from database
+            messages = data;
+            return db.getUsers();
+          }, err => reject(err))
+          .then(data => {
+            //pull users from database
+            users = data;
+            return db.getRooms();
+          }, err => reject(err))
+          .then(rooms =>{
+            //pull rooms from database
+            resolve(messages.map(message => {
+              //transform database message object into a chatterbox message object
+              message.username = users.filter(user => user.id = message.user)[0].name;
+              message.roomname = rooms.filter(room => room.id = message.room)[0].name;
+              message.createdAt = new Date(message.time).toJSON();
+              message.updatedAt = message.createdAt;
+              delete message.user;
+              delete message.room; 
+              delete message.time;
+              return message;
+            }));
+          }, err => reject(err));
+      });
+    },
+    post(message, time = new Date().getTime()) {
+      return new Promise((resolve, reject) => {
+        //post message to database
+        db.postMessage({text: message.message, user: message.username, room: message.roomname, time})
+          .then(id => {
+            //if post successful, return back response object
+            id ? resolve({
+              objectId: time,
+              createdAt: new Date(time).toJSON()
+            }) : reject(id);
+          }, err => reject(err));
+      });
+    }
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get() {
+      return db.getUsers();
+    },
+    post(user) {
+      return db.addUser(user.username);
+    }
   }
 };
 
